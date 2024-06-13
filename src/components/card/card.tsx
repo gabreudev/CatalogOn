9efd5a41678a7de9deccddef7a2e6df62a184e7b
gabriel <biel0alves0@gmail.com>
2024-06-13 16:22:25 -0300

    fix: detailsproduct

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {CartItem} from './../../interface/carrinho'
 
 interface ProductProps {
   id: number;
@@ -11,14 +10,8 @@ interface ProductProps {
 }
 
 const Product: React.FC<ProductProps> = ({ id, image, price, title, description, onClick }) => {
-  const addToCart = (product: Omit<ProductProps, 'onClick'>) => {
-    const cart: CartItem[] = JSON.parse(localStorage.getItem('cart') || '[]');
-    cart.push(product);
-    localStorage.setItem('cart', JSON.stringify(cart));
-  };
-
   return (
-    <div className="col mb-5">
+    <div className="col mb-5" onClick={() => onClick(id)} style={{ cursor: 'pointer' }}>
       <div className="card h-100">
         <img 
           className="card-img-top" 
@@ -29,13 +22,14 @@ const Product: React.FC<ProductProps> = ({ id, image, price, title, description,
         <div className="card-body p-4">
           <div className="text-center">
             <h5 className="fw-bolder">{title}</h5>
+            <p className="card-text">{description}</p>
             <span>R$ {price.toFixed(2)}</span>
           </div>
         </div>
         <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
           <div className="text-center">
-            <button className="btn btn-outline-dark mt-auto" style={{ marginRight: '10px' }} onClick={() => addToCart({ id, image, price, title, description })}>Add to Cart</button>
-            <button className="btn btn-primary mt-auto" onClick={() => onClick(id)}>Comprar</button>
+            <button className="btn btn-outline-dark mt-auto" style={{ marginRight: '10px' }}>Add to Cart</button>
+            <button className="btn btn-primary mt-auto">Comprar</button>
           </div>
         </div>
       </div>
